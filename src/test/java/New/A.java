491f101a24e7c10b1b5520bd9b3c6c6c23724064
satyajit1797 <satyajitgadekar.sg@gmail.com>
2022-06-28 15:28:28 +0530

    commit

---
+++ src/test/java/New/A.java
@@ -0,0 +1,29 @@
+package New;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class A
+{
+  public static void main(String []args) throws InterruptedException
+  {
+	  System.setProperty("webdriver.chrome.driver","E:\\chrome driver\\chromedriver_win32\\chromedriver.exe");
+	  WebDriver driver=new ChromeDriver();
+	  driver.get("https://demoqa.com/");
+      driver.manage().window().maximize();
+      driver.findElement(By.xpath("(//div[@class='card-body'])[1]")).click();
+      Thread.sleep(2000);
+      driver.findElement(By.xpath("(//li[@id='item-4'])[1]")).click();
+      Thread.sleep(2000);
+      WebElement next=driver.findElement(By.id("rightClickBtn"));
+      Actions act=new Actions(driver);
+      act.moveToElement(next).perform();
+      act.contextClick().perform();
+      Thread.sleep(2000);
+  }
+  
+}
+
